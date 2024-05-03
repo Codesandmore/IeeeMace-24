6054fd7926d18ee437e5c57564524da9de97288f
BifinJ <jamesbifin@gmail.com>
2024-05-04 01:28:29 +0530

    chapters commit

---
+++ src/Pages/Chapters/Chapters.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Chapters.css";
+
+export default function Chapters() {
+  return (
+    <div className="chapters">
+      <h1>Chapters</h1>
+    </div>
+  );
+}
