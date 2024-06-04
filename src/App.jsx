4c7021d3b9111ff13cf0d3ca62aa964df9e56b17
Codesandmore <chackochanme@gmail.com>
2024-06-04 19:58:49 +0530

    Exe3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import "./App.css";
-import Chapters from "./Pages/Chapters/Chapters";
-import About from "./Pages/About/About";
-import Events from "./Pages/Events/events";
-import Execom from "./Pages/Execom/Execom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Chapters from './Pages/Chapters/Chapters';
+import About from './Pages/About/About';
+import Events from './Pages/Events/events';
+import Execom from './Pages/Execom/Execom';
+import ExecomFull from './Pages/Execom/ExecomFull';
 
 function App() {
   return (
-    <section className="main">
-      <About />
-      <Chapters />
-      <Events />
-      <Execom />
-    </section>
+    <Router>
+      <section className="main">
+        <Routes>
+          <Route path="/" element={
+            <>
+              <About />
+              <Chapters />
+              <Events />
+              <Execom />
+            </>
+          } />
+          <Route path="/execom" element={<ExecomFull />} />
+        </Routes>
+      </section>
+    </Router>
   );
 }
 
